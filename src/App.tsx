2e50f403270b468427591150e7c141df5c665ead
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-12 11:11:19 -0600

    Connects form in NewTodo to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
 
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
